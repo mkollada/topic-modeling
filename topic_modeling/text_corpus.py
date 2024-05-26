7dc6c46574d312489f41817c7f3baa469c5e15a7
Matt Kollada <mattkollada@gmail.com>
2024-05-26 13:18:18 -0400

    text corpus updates filepaths when it processes or skips so filepaths will have correct set of files

--- topic_modeling/text_corpus.py
+++ topic_modeling/text_corpus.py
@@ -38,10 +38,27 @@ class TextCorpus:
         self.dictionary = corpora.Dictionary()
         self.no_below = no_below
         self.no_above = no_above
-        self.filepaths = [os.path.join(directory, filename) for filename in os.listdir(directory) if filename.endswith(('.pdf', '.txt'))]
-        if max_files is not None:
-            self.filepaths = self.filepaths[:max_files]
+        self.filepaths = []
+        self.skipped_filepaths = []
         self.preprocessed_texts: Dict[str, List[str]] = {}
+
+        all_filepaths = [os.path.join(directory, filename) for filename in os.listdir(directory) if filename.endswith(('.pdf', '.txt'))]
+        if max_files is not None:
+            all_filepaths = all_filepaths[:max_files]
+
+        for filepath in all_filepaths:
+            ext = os.path.splitext(filepath)[1].lower()
+            if ext == '.pdf':
+                text = get_text_from_pdf(filepath)
+            elif ext == '.txt':
+                text = get_text_from_txt(filepath)
+            if text:
+                tokens = preprocess_text(text)
+                if not is_memory_usage_high():
+                    self.preprocessed_texts[filepath] = tokens
+                self.filepaths.append(filepath)
+            else:
+                self.skipped_filepaths.append(filepath)
 
     def __iter__(self) -> Generator[List[tuple], None, None]:
         """
@@ -51,37 +68,26 @@ class TextCorpus:
             Generator[List[tuple], None, None]: A generator that yields the BoW representation of each document.
         """
         for filepath in self.filepaths:
-            if filepath in self.preprocessed_texts:
-                yield self.dictionary.doc2bow(self.preprocessed_texts[filepath])
-            else:
-                ext = os.path.splitext(filepath)[1].lower()
-                if ext == '.pdf':
-                    text = get_text_from_pdf(filepath)
-                elif ext == '.txt':
-                    text = get_text_from_txt(filepath)
-                if text:
-                    tokens = preprocess_text(text)
-                    if not is_memory_usage_high():
-                        self.preprocessed_texts[filepath] = tokens
-                    yield self.dictionary.doc2bow(tokens)
+            yield self.dictionary.doc2bow(self.preprocessed_texts[filepath])
+
+    def __len__(self) -> int:
+        """
+        Returns the number of documents in the corpus.
+        
+        Returns:
+            int: The number of documents.
+        """
+        return len(self.filepaths)
 
     def build_dictionary(self) -> None:
         """
         Builds the dictionary from the documents in the corpus.
         """
         for filepath in tqdm(self.filepaths, desc="Building dictionary", unit="file", leave=False):
-            ext = os.path.splitext(filepath)[1].lower()
-            if ext == '.pdf':
-                text = get_text_from_pdf(filepath)
-            elif ext == '.txt':
-                text = get_text_from_txt(filepath)
-            if text:
-                tokens = preprocess_text(text)
-                if not tokens:
-                    logging.warning(f"No tokens found for file: {filepath}")
-                if not is_memory_usage_high():
-                    self.preprocessed_texts[filepath] = tokens
-                self.dictionary.add_documents([tokens])
+            tokens = self.preprocessed_texts[filepath]
+            if not tokens:
+                logging.warning(f"No tokens found for file: {filepath}")
+            self.dictionary.add_documents([tokens])
         logging.info(f"Dictionary size before filtering: {len(self.dictionary)}")
         self.dictionary.filter_extremes(no_below=self.no_below, no_above=self.no_above)  # Filter based on user input
         logging.info(f"Dictionary size after filtering: {len(self.dictionary)}")
@@ -97,16 +103,7 @@ class TextCorpusWithProgress(TextCorpus):
         """
         uncached_filepaths = [filepath for filepath in self.filepaths if filepath not in self.preprocessed_texts]
         for filepath in tqdm(uncached_filepaths, desc="Processing files", unit="file", leave=False):
-            ext = os.path.splitext(filepath)[1].lower()
-            if ext == '.pdf':
-                text = get_text_from_pdf(filepath)
-            elif ext == '.txt':
-                text = get_text_from_txt(filepath)
-            if text:
-                tokens = preprocess_text(text)
-                if not is_memory_usage_high():
-                    self.preprocessed_texts[filepath] = tokens
-                yield self.dictionary.doc2bow(tokens)
+            yield self.dictionary.doc2bow(self.preprocessed_texts[filepath])
 
         for filepath in self.filepaths:
             if filepath in self.preprocessed_texts:
